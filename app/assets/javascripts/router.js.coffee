75aa9ba8e3a1b88c48c43e413a7d970ddb0aaa2e
Awais Ilyas <awais545@gmail.com>
2013-11-07 00:41:34 +0500

    add new entry in the list

---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,8 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+Raffler.Router.map ()->
+	@route 'entries', path: '/'
+
+
+Raffler.EntriesRoute = Ember.Route.extend
+	setupController: (controller) -> controller.set('content', [])
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'e50f50b6f4b239adbeb007e2a8aa7c8da50fe00c822be7aae1cee38aaa2b64fe670d33a1044c458333e44d9d78a746babd64df0081714cd3d9e269eb3812266f'
